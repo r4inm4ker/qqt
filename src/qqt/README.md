f23b1901ee304a347324ebfea810406e99a4bbd2
rain_local <jefri.yeh@outlook.com>
2019-04-14 13:26:46 +1200

    restructure files

--- src/qqt/README.md
+++
@@ -1,2 +0,0 @@
-# qqt
-A python qt wrapper on top of PySide/Qt.py to enable rapid UI creation with less code required.
--- src/qqt/gui.py
+++
@@ -1,6 +0,0 @@
-from .widgets.base import *
-from .widgets.displays import *
-from .widgets.inputs import *
-from .widgets.listview import *
-from .layouts import *
-from .menu import *
